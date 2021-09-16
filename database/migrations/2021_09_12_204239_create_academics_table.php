427401b9f39a19ab12f7ed40bb2493e8a13a3a12
Sherajus-Salehin <66803588+Sherajus-Salehin@users.noreply.github.com>
2021-09-16 17:58:17 +0600

    database

---
+++ database/migrations/2021_09_12_204239_create_academics_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Egulias\EmailValidator\Warning\Comment;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAcademicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('academics', function (Blueprint $table) {
+            $table->id();
+            $table->integer('uid');
+            $table->string('course');
+            $table->double('gpa',3,2);
+            $table->string('comment');
+            $table->integer('semester');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('academics');
+    }
+}
